c832c1e5417be4e5b5dbed7974db7b35dbe657bc
Developer122436 <dimaspektor12@gmail.com>
2023-04-26 19:09:47 +0300

    Update My Credential In Birthdays Wisher Script

--- Python Projects Through Courses/Section 32 - Pro Bootcamp SMTP/main.py
+++ Python Projects Through Courses/Section 32 - Pro Bootcamp SMTP/main.py
@@ -17,16 +17,18 @@ birthdays_dict = df.set_index(['month', 'day']).to_dict(orient='index')
 if today in birthdays_dict:
     birthday_person = birthdays_dict[today]
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    with open(file_path) as letter_file:
+    with open(file_path, encoding="utf-8") as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person['name'])
+
+    msg = f"Subject: יום הולדת שמח!!\n\n{contents}".encode('utf-8')
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(from_addr=MY_EMAIL,
                             to_addrs=birthday_person["email"],
-                            msg=f"Subject:Happy Birthday!\n\n{contents}")
+                            msg=msg)
 
 # Mails: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
 

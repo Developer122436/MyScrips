ba55c71f9751d588afc929721fdfe21a6c5b404d
Developer122436 <dimaspektor12@gmail.com>
2023-04-27 08:53:51 +0300

    Update My Credential In Birthdays Wisher Script

--- ExtractPDFLamas/ExtractingPDFLamas.py
+++ ExtractPDFLamas/ExtractingPDFLamas.py
@@ -19,13 +19,13 @@ now = dt.datetime.now()
 
 year = now.year - 1
 
-dfToCSV = pd.DataFrame(columns=[f"תקרה {year}", f"שיעור {year}", "שם יישוב", "סמל היישוב"])
+df_to_csv = pd.DataFrame(columns=[f"תקרה {year}", f"שיעור {year}", "שם יישוב", "סמל היישוב"])
 
-firstRow = {f"תקרה {year}": f"תקרה {year}", f"שיעור {year}": f"שיעור {year}", 'שם יישוב': 'שם יישוב', 'סמל היישוב': 'סמל היישוב'}
+first_row = {f"תקרה {year}": f"תקרה {year}", f"שיעור {year}": f"שיעור {year}", 'שם יישוב': 'שם יישוב', 'סמל היישוב': 'סמל היישוב'}
 
-dfToCSV.loc[-1] = firstRow
-dfToCSV.index = dfToCSV.index + 1
-dfToCSV = dfToCSV.sort_index()
+df_to_csv.loc[-1] = first_row
+df_to_csv.index = df_to_csv.index + 1
+df_to_csv = df_to_csv.sort_index()
 
 for table in tables:
     df = table.df
@@ -35,13 +35,13 @@ for table in tables:
 
     df["שם יישוב"] = df["שם יישוב"].str.replace('\n', '')
 
-    dfToCSV = pd.concat([dfToCSV, df.iloc[1:, :]])
+    df_to_csv = pd.concat([df_to_csv, df.iloc[1:, :]])
 
-dfToCSV['שם יישוב'] = dfToCSV['שם יישוב'].replace({'-טלאל': 'טל-אל', 'נהרייה': 'נהריה'})
+df_to_csv['שם יישוב'] = df_to_csv['שם יישוב'].replace({'-טלאל': 'טל-אל', 'נהרייה': 'נהריה'})
 
 if not os.path.exists(sys.argv[2]):
     print("Invalid path!")
 else:
-    dfToCSV.to_csv(
+    df_to_csv.to_csv(
         sys.argv[2],
         header=False, index=False, encoding='utf-8-sig')

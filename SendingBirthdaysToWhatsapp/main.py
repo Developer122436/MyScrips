1cbfe3ffb7d43c0672b8985f42ab68363ff2a0a4
Developer122436 <dimaspektor12@gmail.com>
2023-05-08 22:29:52 +0300

    Update

---
+++ SendingBirthdaysToWhatsapp/main.py
@@ -0,0 +1,35 @@
+import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+from datetime import datetime
+import random
+import pandas as pd
+
+load_dotenv()
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+
+today = (datetime.now().month, datetime.now().day)
+
+df = pd.read_csv('birthdays.csv', encoding='utf-8')
+
+birthdays_dict = df.set_index(['month', 'day']).to_dict(orient='index')
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path, encoding="utf-8") as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person['name'])
+        contents = contents.replace("[NOTES]", birthday_person['notes'])
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=contents,
+        to=f"whatsapp:+972{birthday_person['phone']}"
+    )
+    print(message.status)

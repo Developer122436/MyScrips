b0fed7ffa4083ac02b7ff67fbda522f6eb925497
Developer122436 <dimaspektor12@gmail.com>
2023-05-25 18:39:33 +0300

    Update File Moving To OneDrive Folder

--- MoveFilesBAK.py
+++ MoveFilesBAK.py
@@ -1,10 +1,14 @@
+import os
 import shutil
 
 # Source file path
 source_file = r'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\Settlements.bak'
 
 # Destination folder path
-destination_folder = r'C:\Users\dimas\OneDrive\Desktop\Programming\Data Analyst\BAK Files'
+destination_file = r'C:\Users\dimas\OneDrive\Desktop\Programming\Data Analyst\BAK Files\Settlements.bak'
 
-# Transfer the file
-shutil.move(source_file, destination_folder)
+# Copy the source file to the destination folder
+shutil.copy2(source_file, destination_file)
+
+# Replace the existing file in the destination folder
+os.replace(source_file, destination_file)
